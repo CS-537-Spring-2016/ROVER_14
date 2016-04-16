bd144c10362683f91430f1981db29500cc8b6447
sabhyagupta <sabhya60992@gmail.com>
2016-04-16 15:06:08 -0700

    rover is up

--- src/common/RoverLocations.java
+++ src/common/RoverLocations.java
@@ -35,7 +35,8 @@ public class RoverLocations {
 		
 		// test rovers
 		roverHash.put(RoverName.ROVER_00, new Coord(4,20));
-		roverHash.put(RoverName.ROVER_99, new Coord(4,7));
+		roverHash.put(RoverName.ROVER_14, new Coord(9,9));
+		
 	}
 	
 	public synchronized boolean moveRover(RoverName rname, Coord loc){
